25b4a0febdf700ad3e0ba5abd1b65790fd991b59
Lin <mt5718214@gmail.com>
2020-09-12 11:19:55 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.render('index')
 })
 
 
