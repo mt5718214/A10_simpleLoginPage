b462547388c00c4c005f903463c9e3ca798722a3
Lin <mt5718214@gmail.com>
2020-09-12 14:54:52 +0800

    feat: add condition check if data exist

--- routes/modules/login.js
+++ routes/modules/login.js
@@ -3,14 +3,21 @@ const route = express.Router()
 
 const verifyUsersData = require('../../verifyUsersData')
 
+let fail = false
+
 route.get('/', (req, res) => {
-  return res.render('index')
+  return res.render('index', { fail: fail })
 })
 
 route.post('/login', (req, res) => {
   const userInfo = req.body
   const user = verifyUsersData(userInfo)
-  res.redirect(`/user/${user.firstName}`)
+  if (user) {
+    res.redirect(`/user/${user.firstName}`)
+  } else {
+    fail = true
+    res.redirect('/')
+  }
 })
 
 module.exports = route
